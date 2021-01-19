264da4a78d112b9541ff1a39a7f3e62c744778f2
nadal12 <nadal12@hotmail.com>
2021-01-19 13:26:43 +0100

    Las dos simulaciones funcionan. Comentarios añadidos. Listo para grabar vídeo de demostración.

--- src/Oxygen.java
+++ src/Oxygen.java
@@ -1,18 +1,16 @@
 public class Oxygen implements Runnable {
 
-    private static final int SYNTHESIS_NUMBER = 4;
-    private static final int MAX_TIME_TO_PRODUCE_WATER = 3500;
-    private static final int CHARACTERS_NUMBER = 4;
-    private static final char CHARACTER_OXYGEN_1 = '*';
-    private static final char CHARACTER_OXYGEN_2 = '+';
+    private static final int SYNTHESIS_NUMBER = 4; //Nombre de síntesis que farà cada molècula.
+    private static final int MAX_TIME_TO_PRODUCE_WATER = 3500; //Temps màxim que es tardarà a sintetitzar aigua.
+    private static final int CHARACTERS_NUMBER = 4; //Nombre de caràcters que ha de imprimir cada molècula.
+    private static final char CHARACTER_OXYGEN_1 = '*'; //Caràcter del primer oxigen.
+    private static final char CHARACTER_OXYGEN_2 = '+'; //Caràcter del segon oxigen.
     private final int character;
-    private final int id;
-    private final int hydrogenMolecules;
+    private final int id; //Identificador de la molècula.
 
-    public Oxygen(int id, int hydrogenMolecules) {
+    public Oxygen(int id) {
         this.id = id;
-        this.hydrogenMolecules = hydrogenMolecules;
-        character = (id == 1) ? CHARACTER_OXYGEN_1 : CHARACTER_OXYGEN_2;
+        character = (id == 1) ? CHARACTER_OXYGEN_1 : CHARACTER_OXYGEN_2; //S'assigna el caràcter depenent de la molècula.
     }
 
     @Override
@@ -28,33 +26,50 @@ public class Oxygen implements Runnable {
         System.out.println("L'Oxigen 0x" + id + " acaba");
     }
 
+    /**
+     * Mètode per amollar els hidrogens utilitzats per fer aigua.
+     */
     private void releaseHydrogens() {
         for (int i = 0; i < 2; i++) {
             waterSynthesis.waitSecondHydrogen.release();
         }
     }
 
+    /**
+     * Mètode per imprimir caràcters corresponents a la sintetització d'aigua de cada molècula.
+     */
     private void printCharacters() {
         for (int i = 0; i < CHARACTERS_NUMBER; i++) {
             System.out.print(Character.toString(character) + " ");
+            sleep(2000);
         }
         System.out.println();
     }
 
+    /**
+     * Mètode que sintetitza aigua. Crida a la funció d'imprimir caràcters.
+     */
     private void synthesizeWater() {
-        sleep((long) (Math.random() * MAX_TIME_TO_PRODUCE_WATER));
+        sleep((long) (Math.random() * MAX_TIME_TO_PRODUCE_WATER)); //Sintetització de l'aigua.
         System.out.println("-----------> L'Oxigen Ox" + id + " sintetitza aigua");
         printCharacters();
     }
 
+    /**
+     * Mètode per esperar a que els hidrogens necessaris arribin.
+     */
     private void waitForHydrogens() {
         try {
-            waterSynthesis.waitForHydrogens.acquire();
+            waterSynthesis.waitForHydrogens.acquire(); //Espera a que arribin dos hidrogens.
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * Mètode que fa una espera al fil que el crida.
+     * @param ms Temps d'espera amb ms.
+     */
     private void sleep(long ms) {
         try {
             Thread.sleep(ms);
--- src/waterSynthesis.java
+++ src/waterSynthesis.java
@@ -2,12 +2,24 @@ import java.util.concurrent.Semaphore;
 
 public class waterSynthesis {
 
-    private static final int OXYGEN_MOLECULES = 2;
-    private static final int HYDROGEN_MOLECULES = 4;
+    private static final int OXYGEN_MOLECULES = 2; //Quantitat de molècules d'oxigen.
+    private static final int HYDROGEN_MOLECULES = 4; //Quantitat de molècules d'hidrogen.
 
+    /* DECLARACIÓ DELS SEMÀFORS
+    * waitForHydrogens -> Semàfor que s'utilitza per bloquejar a l'oxigen perque esperi als dos hidrogens.
+    *                     S'inicialitza sense permisos perque no ha de entrar cap oxigen fins que no hagin
+    *                     arribat els dos hidrogens.
+    *
+    * waitSecondHydrogen -> Semàfor per bloquejar el primer hidrogen perque esperi al segon i despres per també
+    *                       bloquejar el segon mentres espera que es faci la sintetització d'aigua.
+    *
+    * mutex -> Semàfor comptador per protegir la variable de comptador d'hidrogens, per diferenciar entre senar i
+    *          parell. S'inicialitza amb un permís perque ens interessa que entri una única molècula dins la secció
+    *          crítica.
+    */
     public static Semaphore waitForHydrogens = new Semaphore(0);
     public static Semaphore waitSecondHydrogen = new Semaphore(0);
-    public static final Semaphore mutex = new Semaphore(1);
+    public static Semaphore mutex = new Semaphore(1);
 
     public static void main(String[] args) {
 
@@ -15,24 +27,46 @@ public class waterSynthesis {
 
         System.out.println("Simulació Sintetitzant Aigua");
 
+        //Creació de les molècules d'oxigen.
         for (int i = 0; i < OXYGEN_MOLECULES; i++) {
-            molecules[i] = new Thread(new Oxygen(i + 1, HYDROGEN_MOLECULES));
+            molecules[i] = new Thread(new Oxygen(i + 1));
             molecules[i].start();
         }
 
+        //Petita espera entre creació de molècules.
+        sleep(100);
+
+        //Creació de les molècules d'hidrogen.
         for (int i = OXYGEN_MOLECULES; i < OXYGEN_MOLECULES + HYDROGEN_MOLECULES; i++) {
             molecules[i] = new Thread(new Hydrogen(i - OXYGEN_MOLECULES + 1));
             molecules[i].start();
         }
 
+        //Esperar a que tots els fils arribin a aquest punt per continuar.
         for (Thread molecule : molecules) {
             join(molecule);
         }
     }
 
+    /**
+     * Mètode que realitza el join del fil que es passa per paràmetre.
+     * @param thread Fil que es vol fer el join.
+     */
     private static void join(Thread thread) {
         try {
             thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Mètode que fa una espera al fil que el crida.
+     * @param ms Temps d'espera amb ms.
+     */
+    private static void sleep(long ms) {
+        try {
+            Thread.sleep(ms);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
